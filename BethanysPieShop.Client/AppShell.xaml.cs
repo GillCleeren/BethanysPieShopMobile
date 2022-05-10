4debe320403a44803e657e7dfd52a732d449686a
Kris Hermans <kris.hermans@live.be>
2022-05-10 18:43:10 +0200

    Added Hello World Maui

---
+++ BethanysPieShop.Client/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BethanysPieShop.Client
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

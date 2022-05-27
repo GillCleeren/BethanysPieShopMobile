9996140eca9b92e3107a14dfacae0f5508a4f86a
Kris Hermans <kris.hermans@live.be>
2022-05-27 21:47:27 +0200

    Working on header and footer

--- BethanysPieShop.Client/Constants/ApiConstants.cs
+++ BethanysPieShop.Client/Constants/ApiConstants.cs
@@ -8,8 +8,14 @@ namespace BethanysPieShop.Client.Constants
 {
     public class ApiConstants
     {
+#if WINDOWS10_0_17763_0_OR_GREATER
+        public const string BaseImagesUrl = "http://localhost:5002/images/";
+        public const string BaseApiUrl = "http://localhost:5000/";
+#else
         public const string BaseImagesUrl = "http://10.0.2.2:5002/images/";
         public const string BaseApiUrl = "http://10.0.2.2:5000/";
+#endif
+
         //public const string BaseApiUrl = "https://bps-pxl.azurewebsites.net/";
         public const string CatalogEndpoint = "api/catalog/pies/";
         public const string PiesOfTheWeekEndpoint = "api/catalog/piesoftheweek/";
--- BethanysPieShop.Client/ViewModels/PieCatatalogViewModel.cs
+++ BethanysPieShop.Client/ViewModels/PieCatatalogViewModel.cs
@@ -15,9 +15,10 @@ public class PieCatalogViewModel : ViewModelBase
 
     public ICommand LoadPiesCommand { get; }
 
-    public void OnAppearing()
+    public async void OnAppearing()
     {
-        IsBusy = true;
+        //IsBusy = true;
+        Pies = (await _catalogDataService.GetAllPiesAsync()).ToObservableCollection();
     }
 
     public PieCatalogViewModel(ICatalogDataService catalogDataService)
@@ -25,6 +26,9 @@ public class PieCatalogViewModel : ViewModelBase
     {
         _catalogDataService = catalogDataService;
         LoadPiesCommand = new Command(async () => await ExecuteLoadPiesCommand());
+        //_pies = new ObservableCollection<Pie>();
+        //var pie = new Pie() { Name = "Apple Pie", Price = 12.95M, ShortDescription = "Our famous apple pies!", LongDescription = "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.", ImageUrl = "applepie.jpg", InStock = true, IsPieOfTheWeek = true, ImageThumbnailUrl = "applepiesmall.jpg", AllergyInformation = "" };
+        //_pies.Add(pie);
     }
 
     private async Task ExecuteLoadPiesCommand()

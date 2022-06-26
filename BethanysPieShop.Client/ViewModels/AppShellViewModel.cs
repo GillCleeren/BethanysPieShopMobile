ce4f66a22629cba7757ce7649fa00a1953f74e37
Kris Hermans <kris.hermans@live.be>
2022-06-26 19:28:48 +0200

    Added logout

--- BethanysPieShop.Client/ViewModels/AppShellViewModel.cs
+++ BethanysPieShop.Client/ViewModels/AppShellViewModel.cs
@@ -2,6 +2,7 @@
 using BethanysPieShop.Client.Contracts.Services.Data;
 using BethanysPieShop.Client.Contracts.Services.General;
 using BethanysPieShop.Client.ViewModels.Base;
+using System.Windows.Input;
 
 namespace BethanysPieShop.Client.ViewModels;
 
@@ -10,25 +11,16 @@ public class AppShellViewModel : ViewModelBase
     private string _greeting;
     private bool _isAuthenticated;
 
-<<<<<<< HEAD
-    //private readonly IAuthenticationService _authenticationService;
     private readonly ISettingsService _settingsService;
 
     public AppShellViewModel(INavigationService navigationService, ISettingsService settingsService)
         : base(navigationService)
     {
         _settingsService = settingsService;
-=======
-    private readonly IAuthenticationService _authenticationService;
-   
-    public AppShellViewModel(INavigationService navigationService, IAuthenticationService authenticationService)
-        : base(navigationService)
-    {
-        _authenticationService = authenticationService;
->>>>>>> 5b74a9c6ade73fe371ecde49ec28bb1d46afffe5
-
         MessagingCenter.Subscribe<LoginViewModel>(this, MessagingConstants.LoginSucceeded, (loginViewModel) => OnLoginSucceeded());
     }
+
+    public ICommand LogoutCommand => new Command(OnLogout);
 
     
     public string Greeting
@@ -46,7 +38,6 @@ public class AppShellViewModel : ViewModelBase
 
     public override async void OnAppearing()
     {
-        Greeting = "Well, hello there!";
         IsAuthenticated = !string.IsNullOrEmpty(_settingsService.UserIdSetting);
 
         if (!IsAuthenticated)
@@ -55,22 +46,22 @@ public class AppShellViewModel : ViewModelBase
         }
         else
         {
+            Greeting = $"Hello, {_settingsService.UserNameSetting}!";
             await _navigationService.NavigateAsync(NavigationConstants.Home);
         }
     }
 
     private void OnLoginSucceeded()
     {
-<<<<<<< HEAD
         IsAuthenticated = !string.IsNullOrEmpty(_settingsService.UserIdSetting);
         Greeting = $"Hello, {_settingsService.UserNameSetting}!";
     }
-=======
-        IsAuthenticated = _authenticationService.IsUserAuthenticated();
-        Greeting = "Yo!";
-    }
 
-    // ToDo: Subscribe to message Authentication Succes and update Greeting
-    // ToDo: Make IsAuthenticated a property that you can raise => update UI
->>>>>>> 5b74a9c6ade73fe371ecde49ec28bb1d46afffe5
+    private async void OnLogout()
+    {
+        _settingsService.UserIdSetting = String.Empty;
+        _settingsService.UserNameSetting = String.Empty;
+        IsAuthenticated = false;
+        await _navigationService.NavigateAsync(NavigationConstants.Login);
+    }
 }
